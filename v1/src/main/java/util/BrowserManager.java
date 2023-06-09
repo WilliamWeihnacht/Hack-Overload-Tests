eda8594647dacb785089d3a167af08bba2de2796
Will Weihnacht <wwhynot@terpmail.umd.edu>
2023-06-08 18:25:20 -0700

    refactored some stuff and added exceptions

---
+++ v1/src/main/java/util/BrowserManager.java
@@ -0,0 +1,25 @@
+package util;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrowserManager {
+	
+	public static WebDriver getDriver(String type) {
+		WebDriver driver = null;
+		if (type.equalsIgnoreCase("chrome")) {
+			String path = System.getProperty("user.dir");
+			System.setProperty("webdriver.chrome.driver",path+"/drivers/chromedriver");
+			driver = new ChromeDriver();
+		} else if (type.equalsIgnoreCase("firefox")) {
+			//driver = new firefox driver TODO
+		} else {
+			throw new IllegalStateException();
+		}
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		return driver;
+	}
+}
